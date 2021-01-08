5d648898933f34cb30c67d3137a0ab0da2ee1104
Jens <Jens@Hermannstaedter.eu>
2021-01-08 21:19:29 +0100

    Update Docker_Install_alpha.sh

--- Docker_Install_alpha.sh
+++ Docker_Install_alpha.sh
@@ -1,8 +1,11 @@
-sudo apt update && sudo apt upgrade -y && sudo apt -y install docker-compose
+sudo apt update && sudo apt upgrade -y
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
 sudo usermod -aG docker pi
+
+sudo apt -y install docker-compose
+wget https://github.com/FinsTar/MeinDockerPi/blob/main/docker-compose.yml
 
 sudo reboot
