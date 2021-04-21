9b22056f2c2df33faad85d53ec9df2cc472dd360
Jens <Jens@Hermannstaedter.eu>
2021-04-21 20:45:57 +0200

    Update Docker_Install_alpha.sh
    bug aus docker-compose.yml Download entfernt

--- Docker_Install_alpha.sh
+++ Docker_Install_alpha.sh
@@ -6,6 +6,6 @@ sudo sh get-docker.sh
 sudo usermod -aG docker pi
 
 sudo apt -y install docker-compose
-wget https://github.com/FinsTar/MeinDockerPi/blob/main/docker-compose.yml
+wget https://raw.githubusercontent.com/FinsTar/MeinDockerPi/main/docker-compose.yml
 
 sudo reboot
