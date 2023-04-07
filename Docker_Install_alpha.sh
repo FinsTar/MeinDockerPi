6ea534d2a3c44251d53d53ee766cedcaa98a2bbe
FinsTar <Jens@Hermannstaedter.eu>
2023-04-07 12:32:08 +0200

    Update Docker_Install_alpha.sh
    User change

--- Docker_Install_alpha.sh
+++ Docker_Install_alpha.sh
@@ -3,7 +3,7 @@ sudo apt update && sudo apt upgrade -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-sudo usermod -aG docker pi
+sudo usermod -aG docker "$USER"
 
 sudo apt -y install docker-compose
 wget https://raw.githubusercontent.com/FinsTar/MeinDockerPi/main/docker-compose.yml
